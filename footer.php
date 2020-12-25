a6200098a7ac4d108251450cbe097a7da678c7cb
SumanRBangera <70790498+SumanRBangera@users.noreply.github.com>
2020-12-25 19:25:27 +0530

    Delete footer.php

--- footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="page-footer">
-          <div class="footer-copyright">
-            <div class="container">
-            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
-            </div>
-          </div>
-</footer>
-</body>
